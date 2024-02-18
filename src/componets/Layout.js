e7f59cd5677d494522519fe86fcf77027fbfdbfb
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2024-02-19 01:11:44 +0530

    Update Layout.js

--- src/componets/Layout.js
+++ src/componets/Layout.js
@@ -35,9 +35,18 @@ const Layout = ({ children, data }) => {
     return (
         <>
             <Head>
-                {facebookPixelScript && (
-                    <script dangerouslySetInnerHTML={{ __html: facebookPixelScript }} />
-                )}
+                    <script dangerouslySetInnerHTML={{ __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '923229855821294');
+fbq('track', 'PageView');
+` }} />
             </Head>
             {facebookPixelScript && (
                 <noscript
